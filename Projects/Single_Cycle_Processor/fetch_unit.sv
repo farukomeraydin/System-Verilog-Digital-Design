ad29d67f66a84451097b1980796944e0cd914ca3
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-02-18 14:57:05 +0100

    Update fetch_unit.sv

--- Projects/Single_Cycle_Processor/fetch_unit.sv
+++ Projects/Single_Cycle_Processor/fetch_unit.sv
@@ -1,1 +1,17 @@
-
+module fetch (
+input logic clk,reset,
+input logic pc_update,
+input logic [31:0] pc_new,
+output logic [31:0] pc
+);
+logic [31:0] pcreg;
+always_ff @ (negedge reset,posedge clk) begin
+pc <= pcreg;
+if (!reset)
+pcreg <= 0;
+else if (pc_update)
+pcreg <= pc_new;
+else
+pcreg <= pcreg + 32'd4;
+end
+endmodule
